702e09994a7b9f8157fadac37efc737c314a103d
JC Kim <tehloo@gmail.com>
2017-01-15 15:41:56 +0900

    Initial Commit

---
+++ EntityScanner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EntityScanner
+//
+//  Created by tehloo on 2017. 1. 15..
+//  Copyright © 2017년 tehloo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
